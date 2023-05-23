0cb1d04f1dd52d9364092987f4afca0d786c2b68
tanvirmhamud <shakilhassan887@gmail.com>
2023-05-23 11:55:00 +0600

    fast commit

--- lib/src/widgets/message_row/message_row.dart
+++ lib/src/widgets/message_row/message_row.dart
@@ -107,62 +107,70 @@ class MessageRow extends StatelessWidget {
             onTap: messageOptions.onPressMessage != null
                 ? () => messageOptions.onPressMessage!(message)
                 : null,
-            child: Column(
-              crossAxisAlignment: isOwnMessage
-                  ? CrossAxisAlignment.end
-                  : CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                if (messageOptions.top != null)
-                  messageOptions.top!(message, previousMessage, nextMessage),
-                if (!isOwnMessage &&
-                    messageOptions.showOtherUsersName &&
-                    (!isPreviousSameAuthor || isAfterDateSeparator))
-                  messageOptions.userNameBuilder != null
-                      ? messageOptions.userNameBuilder!(message.user)
-                      : DefaultUserName(user: message.user),
-                if (message.medias != null &&
-                    message.medias!.isNotEmpty &&
-                    messageOptions.textBeforeMedia)
-                  messageOptions.messageMediaBuilder != null
-                      ? messageOptions.messageMediaBuilder!(
-                          message, previousMessage, nextMessage)
-                      : MediaContainer(
-                          message: message,
-                          isOwnMessage: isOwnMessage,
-                          messageOptions: messageOptions,
-                        ),
-                if (message.text.isNotEmpty)
-                  AnimatedContainer(
-                    duration: duration ?? Duration(seconds: 0),
-                    transform: transform ?? Matrix4.translationValues(0, 0, 0),
-                    child: TextContainer(
-                      messageOptions: messageOptions,
-                      message: message,
-                      previousMessage: previousMessage,
-                      nextMessage: nextMessage,
-                      isOwnMessage: isOwnMessage,
-                      isNextSameAuthor: isNextSameAuthor,
-                      isPreviousSameAuthor: isPreviousSameAuthor,
-                      isAfterDateSeparator: isAfterDateSeparator,
-                      isBeforeDateSeparator: isBeforeDateSeparator,
-                      messageTextBuilder: messageOptions.messageTextBuilder,
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: messageOptions.maxWidth ??
+                    MediaQuery.of(context).size.width * 0.7,
+              ),
+              child: Column(
+                crossAxisAlignment: isOwnMessage
+                    ? CrossAxisAlignment.end
+                    : CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  if (messageOptions.top != null)
+                    messageOptions.top!(message, previousMessage, nextMessage),
+                  if (!isOwnMessage &&
+                      messageOptions.showOtherUsersName &&
+                      (!isPreviousSameAuthor || isAfterDateSeparator))
+                    messageOptions.userNameBuilder != null
+                        ? messageOptions.userNameBuilder!(message.user)
+                        : DefaultUserName(user: message.user),
+                  if (message.medias != null &&
+                      message.medias!.isNotEmpty &&
+                      messageOptions.textBeforeMedia)
+                    messageOptions.messageMediaBuilder != null
+                        ? messageOptions.messageMediaBuilder!(
+                            message, previousMessage, nextMessage)
+                        : MediaContainer(
+                            message: message,
+                            isOwnMessage: isOwnMessage,
+                            messageOptions: messageOptions,
+                          ),
+                  if (message.text.isNotEmpty)
+                    AnimatedContainer(
+                      duration: duration ?? Duration(seconds: 0),
+                      transform:
+                          transform ?? Matrix4.translationValues(0, 0, 0),
+                      child: TextContainer(
+                        messageOptions: messageOptions,
+                        message: message,
+                        previousMessage: previousMessage,
+                        nextMessage: nextMessage,
+                        isOwnMessage: isOwnMessage,
+                        isNextSameAuthor: isNextSameAuthor,
+                        isPreviousSameAuthor: isPreviousSameAuthor,
+                        isAfterDateSeparator: isAfterDateSeparator,
+                        isBeforeDateSeparator: isBeforeDateSeparator,
+                        messageTextBuilder: messageOptions.messageTextBuilder,
+                      ),
                     ),
-                  ),
-                if (message.medias != null &&
-                    message.medias!.isNotEmpty &&
-                    !messageOptions.textBeforeMedia)
-                  messageOptions.messageMediaBuilder != null
-                      ? messageOptions.messageMediaBuilder!(
-                          message, previousMessage, nextMessage)
-                      : MediaContainer(
-                          message: message,
-                          isOwnMessage: isOwnMessage,
-                          messageOptions: messageOptions,
-                        ),
-                if (messageOptions.bottom != null)
-                  messageOptions.bottom!(message, previousMessage, nextMessage),
-              ],
+                  if (message.medias != null &&
+                      message.medias!.isNotEmpty &&
+                      !messageOptions.textBeforeMedia)
+                    messageOptions.messageMediaBuilder != null
+                        ? messageOptions.messageMediaBuilder!(
+                            message, previousMessage, nextMessage)
+                        : MediaContainer(
+                            message: message,
+                            isOwnMessage: isOwnMessage,
+                            messageOptions: messageOptions,
+                          ),
+                  if (messageOptions.bottom != null)
+                    messageOptions.bottom!(
+                        message, previousMessage, nextMessage),
+                ],
+              ),
             ),
           ),
           if (messageOptions.showCurrentUserAvatar)
